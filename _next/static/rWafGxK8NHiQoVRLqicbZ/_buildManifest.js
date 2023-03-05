d599fa38e190eced175d9cb693b577670aa97b1f
Bekir AYDOĞAN <byas66@gmail.com>
2023-03-06 00:34:20 +0300

    new-design fix

---
+++ _next/static/rWafGxK8NHiQoVRLqicbZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d1c1be953fe61cfa.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
